c27e8ac52e245a7a1bc18ce946a966f1e744bb4d
BuildTools <sarquentin@gmail.com>
2022-11-10 11:00:14 +0100

    :tada: Initialize project

---
+++ src/main/java/dev/sarq/projethooks/App.java
@@ -0,0 +1,13 @@
+package dev.sarq.projethooks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
